f86c03df6c8eb4d9167ea48ab9bb368d5d7812d6
Prashanth2025 <prashanthramesh.02@gmail.com>
2025-09-09 16:06:36 +0530

    todohtml

---
+++ DOM/test22.js
@@ -0,0 +1,9 @@
+let fruits = ["Apple", "Banana", "Cherry"];
+let slice = fruits.slice(1, 2);
+console.log(slice);
+let splicec = fruits.splice(1, 2);
+console.log(splicec);
+let add = fruits.push("kapple", "rapple");
+console.log(fruits);
+let slice2 = fruits.splice(0, 1);
+console.log(slice2);
